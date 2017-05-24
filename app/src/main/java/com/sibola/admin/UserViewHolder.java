ef3a192331f5c164c1e12b6b4001971acd7b79c3
faishalnahidha <faishalnahidha@gmail.com>
2017-05-24 16:52:27 +0700

    versi 0.9.0

--- app/src/main/java/com/sibola/admin/UserViewHolder.java
+++ app/src/main/java/com/sibola/admin/UserViewHolder.java
@@ -12,9 +12,11 @@ public class UserViewHolder extends RecyclerView.ViewHolder {
 
     private TextView textUsername;
     private TextView textEmail;
+    private View itemView;
 
     public UserViewHolder(View itemView) {
         super(itemView);
+        this.itemView = itemView;
         this.textUsername = (TextView) itemView.findViewById(R.id.text_username);
         this.textEmail = (TextView) itemView.findViewById(R.id.text_email);
     }
@@ -25,5 +27,9 @@ public class UserViewHolder extends RecyclerView.ViewHolder {
 
     public void setTextEmail(String text) {
         textEmail.setText(text);
+    }
+
+    public View getItemView() {
+        return itemView;
     }
 }
