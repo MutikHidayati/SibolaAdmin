b9ca674e3bd1f90c39313acca7e6569408d31196
Faishal Nahidha <faishalnahidha@gmail.com>
2018-07-27 20:39:59 +0700

    versi 0.9.2 support Android Oreo

--- app/src/main/java/com/sibola/admin/BookingFragment.java
+++ app/src/main/java/com/sibola/admin/BookingFragment.java
@@ -10,7 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
+//import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
